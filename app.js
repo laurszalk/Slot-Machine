19b60a412253be857212f151b35f2e84c23e194f
laurszalk <laurenszalk@gmail.com>
2023-06-26 11:06:18 -0400

    added functions for number of lines and collecting bet

--- app.js
+++ app.js
@@ -13,10 +13,9 @@ const prompt = require("prompt-sync")();
 function deposit() {
   while (true) {
     const depositAmount = prompt("Enter a deposit amount: "); // returns a string
-    const numDepositAmount = parseFloat(depositAmount); //converts string to num
-
+    const numDepositAmount = parseInt(depositAmount); //converts string to num
+    //validation for user input
     if (isNaN(numDepositAmount) || numDepositAmount <= 0) {
-      //validation for user input
       console.log("Invalid amount, try again.");
     } else {
       return numDepositAmount;
@@ -24,10 +23,35 @@ function deposit() {
   }
 }
 
-const depositAmount = deposit();
-
 // 2. determine number of lines to bet on
+function getNumberofLines() {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseInt(lines);
 
-function getNumberofLines(){
-    
-} 
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number, try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+}
+
+// 3. collect bet based on the balance they deposited
+
+function getBet(balance, lines) {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseInt(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet, try again.");
+    } else {
+      return numberBet;
+    }
+  }
+}
+
+let balance = deposit();
+const numberOfLines = getNumberofLines();
+const bet = getBet(balance, numberOfLines);
